dbe699d082f55f3b94b090bba6a977187e9d4f7d
Koulba <thierrykoulba@gmail.com>
2026-05-11 10:35:57 +0200

    Extend poll store

--- resources/js/stores/usePollStore.js
+++ resources/js/stores/usePollStore.js
@@ -10,12 +10,31 @@ export function usePollStore() {
     polls.value = data;
   }
 
-  async function deletePoll(id) {
-    const result = await fetchApi({ url: 'polls/' + id, method: 'DELETE' });
-    if (result) {
-      polls.value = polls.value.filter(p => p.id !== id);
-    }
+  async function loadPolls() {
+    const data = await fetchApi({ url: 'polls' });
+    if (Array.isArray(data)) polls.value = data;
   }
 
-  return { polls, setPolls, deletePoll };
+  async function createPoll(payload) {
+    const created = await fetchApi({ url: 'polls', method: 'POST', data: payload });
+    if (created) polls.value = [created, ...polls.value];
+    return created;
+  }
+
+  async function deletePoll(id) {
+    const result = await fetchApi({ url: 'polls/' + id, method: 'DELETE' });
+    if (result) polls.value = polls.value.filter(p => p.id !== id);
+  }
+
+  async function startPoll(id) {
+    const updated = await fetchApi({ url: `polls/${id}/start`, method: 'POST' });
+    if (updated) replaceInList(updated);
+    return updated;
+  }
+
+  function replaceInList(poll) {
+    polls.value = polls.value.map(p => p.id === poll.id ? poll : p);
+  }
+
+  return { polls, setPolls, loadPolls, createPoll, deletePoll, startPoll };
 }
