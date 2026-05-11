95c92abfa75fe398d86140ed1c9e82760d892fdd
Koulba <thierrykoulba@gmail.com>
2026-05-11 10:51:34 +0200

    Pass auth to the vote view

--- resources/views/polls/vote.blade.php
+++ resources/views/polls/vote.blade.php
@@ -1,5 +1,9 @@
-  <x-vue-app-layout>
-      <x-slot:scripts>@vite(['resources/js/poll-vote.js'])</x-slot>
-      <x-slot:title>Voter</x-slot>
-      <div id="app" data-props='@json(["token" => $token])'></div>
-  </x-vue-app-layout>
+<x-vue-app-layout>
+    <x-slot:scripts>@vite(['resources/js/poll-vote.js'])</x-slot>
+    <x-slot:title>Voter</x-slot>
+    <div id="app" data-props='@json([
+        "token" => $token,
+        "isAuthenticated" => $isAuthenticated,
+        "loginUrl" => $loginUrl,
+    ])'></div>
+</x-vue-app-layout>
